712a7354f4d43012f6c00e0f0e2736ea30eef29c
AlairtonSilva <ahads.pje22@uea.edu.br>
2022-07-29 15:03:31 -0400

    Paralax e rotação do passaro

---
+++ Assets/LoopInfinity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopInfinity : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float textureSpeed;
+
+    void Update(){
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex",GetComponent<Renderer>().material.GetTextureOffset("_MainTex") + new Vector2(textureSpeed,0)*Time.deltaTime);
+    }
+}
