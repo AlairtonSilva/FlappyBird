fb9641f95975de384bcb41eec67684b6297497d1
YonHoshihara <hoshiharyon@gmail.com>
2022-07-29 23:35:40 -0400

    config pipe on base scene

--- Assets/LoopInfinity.cs
+++ Assets/LoopInfinity.cs
@@ -7,7 +7,13 @@ public class LoopInfinity : MonoBehaviour
     [Range(0.0f, 1.0f)]
     public float textureSpeed;
 
+    private Renderer m_Renderer;
+
+    private void Awake()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
     void Update(){
-        GetComponent<Renderer>().material.SetTextureOffset("_MainTex",GetComponent<Renderer>().material.GetTextureOffset("_MainTex") + new Vector2(textureSpeed,0)*Time.deltaTime);
+        m_Renderer.material.SetTextureOffset("_MainTex",GetComponent<Renderer>().material.GetTextureOffset("_MainTex") + new Vector2(textureSpeed,0)*Time.deltaTime);
     }
 }
--- Assets/Script/Obstacles.cs
+++ Assets/Script/Obstacles.cs
@@ -14,7 +14,6 @@ public class Obstacles : MonoBehaviour
     {
         GameObject newPipe=Instantiate(pipe);
         newPipe.transform.position=transform.position+ new Vector3(0, Random.Range(-height,height),0);
-      
     }
 
     // Update is called once per frame
